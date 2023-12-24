7221483b1b087c52f080e6a9730e80fbb8a479ed
hannanabdul123 <abdhannan121@gmail.com>
2023-12-24 17:17:46 +0530

    MysecondAssignment

---
+++ index.js
@@ -0,0 +1,52 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+const itemsArr = ['apple', 'banana'];
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://abdul:abdul@cluster0.z0quves.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+const itemSchema = new mongoose.Schema({
+    name: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.get("/", (req, res) => { res.send('My first backend work'); });
+
+app.get('/fetch', async (req, res) => {
+    try {
+        const allItems = await Item.find();
+        res.json(allItems);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/add', async (req, res) => {
+    try {
+        const newItem = new Item(req.body);
+        const savedItem = await newItem.save();
+        res.json({ message: 'Item added successfully', newItem: savedItem });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
